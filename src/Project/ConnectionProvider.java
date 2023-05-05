b5446c3a6f28728a177105851ef0727b82490848
kumarsheetal51 <kumarsheetal51@gmail.com>
2023-05-05 15:29:55 +0530

    it is comppleted

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import  java.sql.*;
+import  java.util.*;
+
+/**
+ *
+ * @author admin
+ */
+public class ConnectionProvider 
+{
+    
+    public static Connection getCon ()
+    {
+        try{
+          //  Class.forName("com.mysql.jdbc.Driver");
+            Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","Sheetal@420");
+            return con;
+        }
+        catch(Exception e){
+            System.out.println(e);     
+            return null;
+        }
+    }
+    
+    
+}
